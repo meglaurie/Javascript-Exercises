cc4d627c26f90ea435037d7bcca6b96c0420738e
meglaurie <megan.laurie@edu.sait.ca>
2020-10-14 14:37:35 -0600

    Prints to console

--- Exercise-5/index.js
+++ Exercise-5/index.js
@@ -1,22 +1,19 @@
 function fizzBuzz(){
-  // for(let i = 1; i <= 100; i++){
-  //   var result = " ";
-  //   if(i % 3 === 0) {
-  //     return result += "Fizz";
-  //   } if (i % 5 === 0 ) {
-  //     return result += "Buzz";
-  //   } else{
-  //     return result;
-  //   }
-  for (let i = 1; i <= 100; i++) {
-    let result = '';
-    if (i % 3 === 0) result += 'Fizz';
-    if (i % 5 === 0) result += 'Buzz';
-    console.log(result || i);
+  var result = "", x,y,z;
+  for(var z = 1; z <= 100; z++){
+    x = z%3 == 0;
+    y = z%5 == 0;
+    if( x ) {
+      result += "Fizz";
+    } if ( y ) {
+      result += "Buzz";
+    } if (!(x||y)){
+      result +=z;
+    }
+    result +='\n';
     console.log(result);
-
-    document.getElementById('root').innerHTML = result;
   }
+  document.getElementById('root').innerHTML = result;
 }
 
 fizzBuzz();
