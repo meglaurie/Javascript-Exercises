62f5e3735d91d1168cb39b6e807cde3fac365a5b
meglaurie <megan.laurie@edu.sait.ca>
2021-02-06 14:29:40 -0700

    progress

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -5,12 +5,52 @@ function computerPlay(){
     var choice; 
     if( random >= 0 && random <= 0.3){
         choice = 'rock';
-    }if(random >= 0.31 && random <= 0.61){
+    }if(random >= 0.3 && random <= 0.6){
         choice = 'paper';
-    }if(random >= 0.62 && random <= 1){
+    }if(random >= 0.6 && random <= 1){
         choice = 'scissors';
     }
     return choice;
 }
 
 console.log(computerPlay());
+
+var playerSelection = 'ROCK';
+var computerSelection = computerPlay();
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        console.log('player wins');
+    }
+    if(playerSelection === 'rock' && computerSelection === 'paper'){
+        console.log('computer wins');
+    }
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        console.log('tie');
+    }
+    if(playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log('player wins');
+    }
+    if(playerSelection === 'paper' && computerSelection === 'paper'){
+        console.log('tie');
+    }
+    if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        console.log('computer wins');
+    }
+    if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        console.log('computer wins');
+    }
+    if(playerSelection === 'scissors' && computerSelection === 'scissors'){
+        console.log('tie');
+    }
+    if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        console.log('player wins');
+    }
+    
+}
+
+
+playRound(playerSelection, computerSelection);
