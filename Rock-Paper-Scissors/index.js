872a4adbf4b31623cd07e497329c50706ba6fc32
meglaurie <megan.laurie@edu.sait.ca>
2021-02-06 14:11:31 -0700

    rock paper scissors

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,12 +1,16 @@
-function playRound(userInput, compInput){
-
+function computerPlay(){
+    min = Math.ceil(0);
+    max = Math.floor(1);
+    var random = Math.random(min, max);
+    var choice; 
+    if( random >= 0 && random <= 0.3){
+        choice = 'rock';
+    }if(random >= 0.31 && random <= 0.61){
+        choice = 'paper';
+    }if(random >= 0.62 && random <= 1){
+        choice = 'scissors';
+    }
+    return choice;
 }
 
-function compGuess(){
-  
-}
-
-const userInput = "rock";
-const compInput = compGuess();
-
-console.log(playRound(userInput, compInput));
+console.log(computerPlay());
