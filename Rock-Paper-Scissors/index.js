10864c1e595f5bf2cc87068fcf09778d1ba6612a
meglaurie <megan.laurie@edu.sait.ca>
2021-02-06 18:55:50 -0700

    working out bugs

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,19 +1,24 @@
-function computerPlay(){
-    min = Math.ceil(0);
-    max = Math.floor(1);
-    var random = Math.random(min, max);
-    var choice; 
-    if( random >= 0 && random <= 0.3){
-        choice = 'rock';
-    }if(random >= 0.3 && random <= 0.6){
-        choice = 'paper';
-    }if(random >= 0.6 && random <= 1){
-        choice = 'scissors';
-    }
-    return choice;
+// function computerPlay(){
+//     min = Math.ceil(0);
+//     max = Math.floor(1);
+//     var random = Math.random(min, max);
+//     var choice; 
+//     if( random >= 0 && random <= 0.3){
+//         choice = 'rock';
+//     }if(random >= 0.3 && random <= 0.6){
+//         choice = 'paper';
+//     }if(random >= 0.6 && random <= 1){
+//         choice = 'scissors';
+//     }
+//     return choice;
+// }
+
+const myArray = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
 }
 
-console.log(computerPlay());
 
 var playerSelection = 'ROCK';
 var computerSelection = computerPlay();
@@ -22,35 +27,25 @@ var computerSelection = computerPlay();
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
-    if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        console.log('player wins');
-    }
-    if(playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log('computer wins');
-    }
-    if(playerSelection === 'rock' && computerSelection === 'rock'){
+   
+    if(playerSelection ==  computerSelection){
         console.log('tie');
-    }
-    if(playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log('player wins');
-    }
-    if(playerSelection === 'paper' && computerSelection === 'paper'){
-        console.log('tie');
-    }
-    if(playerSelection === 'paper' && computerSelection === 'scissors'){
-        console.log('computer wins');
-    }
-    if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        console.log('computer wins');
-    }
-    if(playerSelection === 'scissors' && computerSelection === 'scissors'){
-        console.log('tie');
-    }
-    if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log('player wins');
+    }else if((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')){
+        console.log("player wins");
+
+    }else if((playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock')){
+            console.log("computer wins");
+        }
+
     }
     
-}
+    
+console.log("player: " + playerSelection);
+console.log("computer: " + computerSelection);
 
 
 playRound(playerSelection, computerSelection);
