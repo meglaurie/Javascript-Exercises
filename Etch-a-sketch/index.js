ae6a8b2326116b55c2cc150bafee524e1a31b265
meglaurie <megan.laurie@edu.sait.ca>
2021-02-12 16:47:09 -0700

    clear button added to Etch a sketch

--- Etch-a-sketch/index.js
+++ Etch-a-sketch/index.js
@@ -1,8 +1,9 @@
 const container = document.getElementById("container");
 const gridItem = document.querySelectorAll('div');
-let clearBtn = document.createElement('button');
+const body = document.querySelector('body');
+const btn = document.createElement('button');
 
-container.appendChild(clearBtn);
+
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -10,7 +11,9 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+
   };
+ body.appendChild(btn);
 };
 
 makeRows(16, 16);
@@ -18,13 +21,21 @@ makeRows(16, 16);
 for( var i = 0; i < gridItem.length; i++){
     gridItem[0].addEventListener("mouseover", function( event ){
         // highlight the mouseenter target
-        event.target.style.background = "black";
-       
-        // reset the color after a short delay
-        setTimeout(function() {
-          event.target.style.background = "";
-        }, 1000);
+        event.target.style.backgroundColor = "black";
        }, false);
-
 }
 
+
+btn.textContent = 'Clear';
+btn.id = "clear";
+
+
+
+document.getElementById("clear").addEventListener("click", function() {
+    let cell = container.childNodes;
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].style.removeProperty('background');
+        
+    }
+        
+ });
